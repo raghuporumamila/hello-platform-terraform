65e717a0c022b6af8ef5f7807efe122650b1d7f7
Suneetha Porumamilla <suneetha.porumamilla@MacBook-Air-3.local>
2026-03-03 12:54:18 -0500

    Adding http external load balancer

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -35,3 +35,44 @@ resource "google_cloud_run_v2_service_iam_member" "public_access" {
   member   = "allUsers"
 }
 
+# 1. Create the Serverless NEG for Cloud Run
+resource "google_compute_region_network_endpoint_group" "serverless_neg" {
+  name                  = "${var.service_name}-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.region
+  cloud_run {
+    service = google_cloud_run_v2_service.platform_service.name
+  }
+}
+
+# 2. Create the Global Backend Service
+resource "google_compute_backend_service" "default" {
+  name                  = "${var.service_name}-backend"
+  protocol              = "HTTP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+
+  backend {
+    group = google_compute_region_network_endpoint_group.serverless_neg.id
+  }
+}
+
+# 3. URL Map to route incoming requests to the backend
+resource "google_compute_url_map" "default" {
+  name            = "${var.service_name}-url-map"
+  default_service = google_compute_backend_service.default.id
+}
+
+# 4. Target HTTP Proxy (For HTTPS, you would use google_compute_target_https_proxy)
+resource "google_compute_target_http_proxy" "default" {
+  name    = "${var.service_name}-http-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+# 5. Global Forwarding Rule (The Public IP)
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "${var.service_name}-forwarding-rule"
+  target                = google_compute_target_http_proxy.default.id
+  port_range            = "80"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+}
+
